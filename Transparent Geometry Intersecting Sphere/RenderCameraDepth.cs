42f923f2e37622ccb4e24eae1be387c3a7690af5
LouisMayor <louismayor1@gmail.com>
2019-05-12 23:50:53 +0100

    added transparent intersection shader

---
+++ Transparent Geometry Intersecting Sphere/RenderCameraDepth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RenderCameraDepth : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Camera renderer = GetComponent<Camera>();
+        if (renderer == null)
+        {
+            Debug.LogError("Failed to find camera");
+            return;
+        }
+
+        renderer.depthTextureMode = DepthTextureMode.DepthNormals;
+    }
+}
